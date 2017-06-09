7fcd5683763ed7bc395391726c14ab3b7746ab24
Hoang Viet <davishoang96@gmail.com>
2017-06-09 21:59:08 +0700

    Added Top Line
    - prevent the plane to fly above the screen
    - added snaptoGrid

---
+++ Assets/Scripts/snapToGrid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class snapToGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public float grid = 0.5f;
+    float x = 0f, y = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grid > 0f)
+        {
+            float new_grid = 1f / grid;
+
+            x = Mathf.Round(transform.position.x * new_grid) / new_grid;
+            y = Mathf.Round(transform.position.y * new_grid) / new_grid;
+
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+    }
+}

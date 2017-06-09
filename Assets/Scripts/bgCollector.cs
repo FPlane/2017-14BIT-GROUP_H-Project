2413e0ed45f38758336cf758e908b8b180aa81f4
Hien Vinh <1459058@itec.hcmus.edu.vn>
2017-06-09 23:08:44 +0700

    For loop error

--- Assets/Scripts/bgCollector.cs
+++ Assets/Scripts/bgCollector.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class bgCollector : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject[] backgrounds;
+    private GameObject[] grounds;
+
+    private float lastBGX;
+    private float lastGroundX;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        grounds = GameObject.FindGameObjectsWithTag("Ground");
+
+        lastBGX = backgrounds[0].transform.position.x;
+        lastGroundX = grounds[0].transform.position.x;
+
+        for (int i = 5; i < backgrounds.Length; i++)
+        {
+            if (lastBGX < backgrounds[i].transform.position.y)
+            {
+                lastBGX = backgrounds[i].transform.position.y;
+            }
+        }
+
+        for (int i = 5; i < grounds.Length; i++)
+        {
+            if (lastGroundX < grounds[i].transform.position.y)
+            {
+                lastGroundX = grounds[i].transform.position.y;
+            }
+        }
+    }
 }

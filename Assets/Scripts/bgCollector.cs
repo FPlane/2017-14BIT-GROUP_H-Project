4a6b66a3ff637fd49b9e4217b87f79628a13ac26
Ngoc Gia <1459015@itec.hcmus.edu.vn>
2017-06-09 23:23:34 +0700

    Finish Bg Collector Script

--- Assets/Scripts/bgCollector.cs
+++ Assets/Scripts/bgCollector.cs
@@ -35,27 +35,25 @@ public class bgCollector : MonoBehaviour {
                 lastGroundX = grounds[i].transform.position.x;
             }
         }
+    }
 
-        void OnTriggerEnter2D(Collider2D target)
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Background")
         {
-            if (target.tag == "Background")
-            {
-                Vector3 temp = target.transform.position;
-                float width = ((BoxCollider2D)target).size.x;
-                temp.x = lastBGX + width;
-                target.transform.position = temp;
-                lastBGX = temp.x;
-            }
-            else if (target.tag == "Ground")
-            {
-                Vector3 temp = target.transform.position;
-                float width = ((BoxCollider2D)target).size.x;
-                temp.x = lastGroundX + width;
-                target.transform.position = temp;
-                lastGroundX = temp.x;
-            }
+            Vector3 temp = target.transform.position;
+            float width = ((BoxCollider2D)target).size.x;
+            temp.x = lastBGX + width;
+            target.transform.position = temp;
+            lastBGX = temp.x;
         }
-
-
+        else if (target.tag == "Ground")
+        {
+            Vector3 temp = target.transform.position;
+            float width = ((BoxCollider2D)target).size.x;
+            temp.x = lastGroundX + width;
+            target.transform.position = temp;
+            lastGroundX = temp.x;
+        }
     }
 }

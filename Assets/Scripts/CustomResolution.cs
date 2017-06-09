b78c747291f0036ce74bfc46892a27f152b5cf8b
Ngoc Gia <1459015@itec.hcmus.edu.vn>
2017-06-09 23:41:14 +0700

    Added custom resolution
    Fixed resolution: 480x800

---
+++ Assets/Scripts/CustomResolution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.fullScreen = !Screen.fullScreen;
+        Screen.SetResolution(480, 800, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

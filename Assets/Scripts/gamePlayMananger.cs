21fafa9220b49a6db5a20ddf6f648b36fcd92e2f
Hien Vinh <1459058@itec.hcmus.edu.vn>
2017-07-01 14:58:37 +0700

    Added gameplay manager script
    - with music background

---
+++ Assets/Scripts/gamePlayMananger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamePlayMananger : MonoBehaviour {
+    public static gamePlayMananger instance;
+    void Awake()
+    {
+        makeSingleIT();
+    }
+
+    void makeSingleIT()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}

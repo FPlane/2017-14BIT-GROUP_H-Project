85a09da833a338143b1dc06c39a5f075a1284a5a
Hien Vinh <1459058@itec.hcmus.edu.vn>
2017-07-01 22:23:44 +0700

    Added reset button

---
+++ Assets/Scripts/resetButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resetButton : MonoBehaviour {
+
+	public void resetGame()
+    {
+        SceneManager.LoadScene("gameplay");
+    }
+}

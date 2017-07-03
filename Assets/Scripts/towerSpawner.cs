e636b6f2ea18b1ba1dd83b0384bd5b4377443062
pttd1304 <pttd1304@gmail.com>
2017-07-03 21:44:19 +0700

    Tower #12

---
+++ Assets/Scripts/towerSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerSpawner : MonoBehaviour {
+
+    [SerializeField]
+
+    private GameObject towerHolder;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawner());
+	}
+	
+	IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(towerHolder, transform.position, Quaternion.identity);
+        StartCoroutine(Spawner());
+    }
+}

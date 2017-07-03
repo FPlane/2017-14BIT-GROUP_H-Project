0cb7ece84e81e2dd59e67b059bbe63d14d508605
pttd1304 <pttd1304@gmail.com>
2017-07-03 15:11:51 +0700

    Add Tower

---
+++ Assets/Scripts/TowerControlling/TowerHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerHolder : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _TowerMovement();
+	}
+
+    void _TowerMovement()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}

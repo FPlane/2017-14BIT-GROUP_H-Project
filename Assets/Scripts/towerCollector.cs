c87bd08aaa635338a3d1aef2bfffe755f7da6dfe
pttd1304 <pttd1304@gmail.com>
2017-07-03 22:46:29 +0700

    Tower issue #12
    - Tower hok thay đổi pos X

---
+++ Assets/Scripts/towerCollector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerCollector : MonoBehaviour
+{
+
+    private GameObject[] tower;
+    private GameObject Tower;
+    private int distance = 10;
+    private float lastTowerX;
+    //private float DropMaxHeight = 5f;
+    //private float DropMinHeight = -5f;
+
+    void Awake()
+    {
+       tower = GameObject.FindGameObjectsWithTag("PipeHolder");//Pipe?
+       for (int i = 0; i < tower.Length; i++)
+       {
+               Vector3 temp = tower[i].transform.position;
+               //temp.y = Random.Range(DropMinHeight, DropMaxHeight);
+               tower[i].transform.position = temp;
+       }
+
+       lastTowerX = tower[0].transform.position.x;
+
+       for (int i = 1; i < tower.Length; i++)
+       {
+            if (lastTowerX < tower[i].transform.position.x)
+            {
+                lastTowerX = tower[i].transform.position.x;
+            }
+       }
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "PipeHolder")
+        {
+            Vector3 temp = target.transform.position;
+            temp.x = lastTowerX + distance;
+            //temp.y = Random.Range(DropMinHeight, DropMaxHeight);
+            target.transform.position = temp;
+            lastTowerX = temp.x;
+        }
+
+
+    }
+
+    void Update()
+    {
+
+    }
+}

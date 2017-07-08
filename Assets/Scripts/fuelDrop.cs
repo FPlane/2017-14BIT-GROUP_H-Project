7b895fbd2afecbc49d641e8cfbadb7a112121317
Hoang Viet <davishoang96@gmail.com>
2017-07-08 15:06:23 +0700

    Update gameplay #19
    if player's fuel is lower than 5
    > give them second change to continue to play by giving them 30 fuel unit > after that fuelunit return to default by setting fuel unit to 5f

--- Assets/Scripts/fuelDrop.cs
+++ Assets/Scripts/fuelDrop.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class fuelDrop : MonoBehaviour {
 
-    public float FuelUnit;
+    private float FuelUnit = 5f;
+
+    // initiation variable once
+    void Start()
+    {
+
+        bool flag = false;
+
+        // if player's fuel is lower than 5
+        // give them second change to continue to play by giving them 30 fuel unit
+        // after that fuelunit return to default by setting fuel unit to 5f
+
+        if(fuel.instance.planeFuel <= 5)
+        {
+            FuelUnit += 30f;
+            flag = true;
+        }
+        if(flag == true)
+        {
+
+            FuelUnit += 5f;
+        }
+    }
 
     // add 2 fuel unit to the plane
     void OnTriggerEnter2D(Collider2D target)

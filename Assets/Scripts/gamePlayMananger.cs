c61fd96f221361fae52486baf6e934e302cc476e
Hoang Viet <davishoang96@gmail.com>
2017-07-09 20:28:27 +0700

    User Interface - update 2
    - Pause button
    - Resume button
    - Reset Button

--- Assets/Scripts/gamePlayMananger.cs
+++ Assets/Scripts/gamePlayMananger.cs
@@ -2,21 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gamePlayMananger : MonoBehaviour {
 
     [SerializeField]
     private Button GuideButton;
 
-    private void Awake()
+    [SerializeField]
+    private Text PauseUI;
+
+    public Button resetButton;
+    public Button pauseButton;
+    public Button resumeButton;
+
+    void Start()
     {
-        Time.timeScale = 0f;
+        
+        Time.timeScale = 0;
+        pauseButton.gameObject.SetActive(false);
+
     }
 
     public void Guide_Button()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1;
         GuideButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(true);
+    }
+
+    public void pause_Button()
+    {
+        Time.timeScale = 0;
+        pauseButton.gameObject.SetActive(false);
+        resumeButton.gameObject.SetActive(true);
+        PauseUI.gameObject.SetActive(true);
+    }
+
+    public void resume_Button()
+    {
+        Time.timeScale = 1;
+        resumeButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(true);
+        PauseUI.gameObject.SetActive(false);
+    }
+
+    public void reset_Button()
+    {
+
+        GuideButton.gameObject.SetActive(false);
+        SceneManager.LoadScene("gameplay");
+        
     }
 
 }

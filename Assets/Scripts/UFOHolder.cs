8baf4924542e5a520ddb4811b3899d1276f3ac7e
pttd1304 <pttd1304@gmail.com>
2017-07-13 20:34:39 +0700

    Write script for UFO Holder #13

---
+++ Assets/Scripts/UFOHolder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOHolder : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _UFOMovement();
+    }
+
+    void _UFOMovement()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+        transform.position = temp;
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}

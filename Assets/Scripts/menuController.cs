ae2605e047a78e498e77ff6c6438aa8620637fb1
Hoang Viet <davishoang96@gmail.com>
2017-07-18 01:13:25 +0700

    Mute Music
    Change button to text with Event Handler

--- Assets/Scripts/menuController.cs
+++ Assets/Scripts/menuController.cs
@@ -4,38 +4,97 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class menuController : MonoBehaviour {
 
     public static menuController instance;
 
-
+    public Text QuitText;
+    public Text SettingText;
 
     public void playButton()
     {
-        SceneManager.LoadScene("guide");
+        SceneManager.LoadScene(2);
     }
 
-    //public void exitButton()
-    //{
-    //    Application.Quit();
-    //}
+    public void setting_button()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
 
     void Awake()
     {
-        makeSingleIT();
+
     }
 
-    void makeSingleIT()
+    
+
+    void Start()
     {
-        if (instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+        SettingText.GetComponent<Text>().color = Color.red;
+        QuitText.GetComponent<Text>().color = Color.red;
+#if UNITY_STANDALONE_WIN
+        print("Game is running on Windows");
+        QuitText.gameObject.SetActive(true);
+#elif UNITY_STANDALONE_OSX
+        print("Game is running on OSX");
+        QuitText.gameObject.SetActive(true);
+#endif
     }
+
+    public void Quit_game()
+    {
+        Application.Quit();
+    }
+    
+    public void setting_game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnEnterQuitText()
+    {
+        print(1);
+        QuitText.GetComponent<Text>().color = Color.white;
+    }
+
+    public void onExitQuitText()
+    {
+        print(2);
+        QuitText.GetComponent<Text>().color = Color.red;
+    }
+
+    public void OnEnterSettingText()
+    {
+        print(1);
+        SettingText.GetComponent<Text>().color = Color.white;
+    }
+
+    public void OnExitSettingText()
+    {
+        print(2);
+        SettingText.GetComponent<Text>().color = Color.red;
+    }
+
+    //void makeSingleIT()
+    //{
+    //    if (instance != null)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //    else
+    //    {
+    //        instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //}
 
 }

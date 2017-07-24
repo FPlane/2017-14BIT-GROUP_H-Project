e50a1b80c3c98cd2742f0bb196866b38d02e91b3
Hoang Viet <davishoang96@gmail.com>
2017-07-24 08:38:39 +0700

    Loading Scene

---
+++ Assets/Scripts/loadingdata.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadingdata : MonoBehaviour {
+    public static loadingdata instance;
+    public int frame_rate = 60;
+	// Use this for initialization
+	void Start () {
+        makeIntance();
+        loading();
+        SceneManager.LoadSceneAsync(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(frame_rate != Application.targetFrameRate)
+        {
+            Application.targetFrameRate = frame_rate;
+        }
+	}
+
+    IEnumerator loading()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(10);
+        print(Time.time);
+    }
+
+    private void makeIntance()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
